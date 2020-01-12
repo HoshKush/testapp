a1c5941a3a73c22e0b87b66be6f746b299dc3cd9
Hosik <orrorin@naver.com>
2020-01-12 16:50:19 +0900

    Updates

---
+++ precache-manifest.8c398c554c160d3a5da47b7710ca5dcb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11fab26891c4bd490cd432d4da0fea19",
+    "url": "/testapp/index.html"
+  },
+  {
+    "revision": "c246ac34a56cf4a593e0",
+    "url": "/testapp/static/css/main.f97739df.chunk.css"
+  },
+  {
+    "revision": "1bffdf681edef9fb052d",
+    "url": "/testapp/static/js/2.6e38fade.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/testapp/static/js/2.6e38fade.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c246ac34a56cf4a593e0",
+    "url": "/testapp/static/js/main.b9858c10.chunk.js"
+  },
+  {
+    "revision": "dbc4a55a6458edb9930a",
+    "url": "/testapp/static/js/runtime-main.d1a284a1.js"
+  }
+]);
